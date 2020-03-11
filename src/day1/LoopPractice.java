398ebd4f3a321ab47cf74db51445ebcd0d0a6d62
TetianaDiaz <tanyushkadiaz@gmail.com>
2020-03-11 19:45:15 -0400

    loop

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -5,6 +5,7 @@ public class LoopPractice {
     public static void main(String[] args) {
         for (int i = 0; i <10 ; i++) {
             System.out.println("bla");
+            System.out.println("#####");
         }
     }
 }
